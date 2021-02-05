22ccfc8712a11f45f1fb0001871b1fdffe4a060e
Sebastian <sebskogen@gmail.com>
2021-02-05 11:30:00 +0100

    Added dunctionality to remove first elements

--- Scripts/analyse_data.py
+++ Scripts/analyse_data.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 from numpy.core.fromnumeric import shape
 import scipy.signal as signal
 
+channels = 4
+
 
 def raspi_import(path, channels=4):
     """
@@ -19,9 +21,10 @@ def raspi_import(path, channels=4):
 
 
 # Import data from bin file
-sample_period, data = raspi_import('export/measurement.bin')
+sample_period, data = raspi_import('export/measurement.bin', channels)
 
-# data = signal.detrend(data, axis=0)  # removes DC component for each channel
+
+data = signal.detrend(data, axis=0)  # removes DC component for each channel
 sample_period *= 1e-6  # change unit to micro seconds
 
 # Generate time axis
@@ -39,13 +42,24 @@ spectrum = np.fft.fft(data, axis=0)  # takes FFT of all channels
 # If you want a single channel, use data[:,n] to get channel n
 # fig = plt.figure(figsize=(16/2.5, 9/2.5))
 
+elements_removed = 100
+
+data_fixed = np.empty([channels, num_of_samples - elements_removed])
+
+
+for i in range(channels):
+    data_fixed[i] = data[:, i][elements_removed:]
+    # data_fixed[i] = i
+print("Fixed", data_fixed)
+
+# print(data[:, 0])
 
 plt.subplot(2, 1, 1)
 plt.title("Time domain signal")
 plt.xlabel("Time [us]")
 plt.ylabel("Voltage")
 plt.grid(True)
-plt.xlim(0.01, .02)
+plt.xlim(0, .02)
 # plt.yticks(np.arange(min(data[:,0]), max(data[:,0])+1, 500))
 plt.plot(t, data[:, :3])
 # 1VA+1V 2.54Vdd, 500Hz
@@ -56,7 +70,7 @@ plt.subplot(2, 1, 2)
 plt.title("Power spectrum of signal")
 plt.xlabel("Frequency [Hz]")
 plt.ylabel("Power [dB]")
-plt.xlim(-2000, 2000)
+plt.xlim(-1000, 1000)
 plt.plot(freq, 20*np.log(np.abs(spectrum)))  # get the power spectrum
 plt.legend(["Ch1$", "Ch2$", "Ch3$"])
 plt.tight_layout()
---
+++ Scripts/tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+ter(10, 15, 'lp', num_of_samples, output='sos')
+
+# data_filtered = []
+
+# for i in range(3):
+
+#     data_filtered.append(signal.butter(sos, data[:, i]))
