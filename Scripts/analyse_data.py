ddcda4e79ba61ae112a79a3415b9644f5de0b867
Asta Skirbekk <asta.skirbekk@outlook.com>
2021-03-05 10:39:39 +0100

    doppler_shift update

--- Scripts/analyse_data.py
+++ Scripts/analyse_data.py
@@ -160,6 +160,11 @@ IQ_freq = np.fft.fftfreq(n=num_interp_samples, d=sample_period_interp)
 doppler_spectrum = np.empty([1, len(IQ_freq)])
 doppler_spectrum[0] = np.fft.fft(combined_IQ, axis=0)
 
+# versjon 2
+doppler_spectrum_v2 = np.fft.fftshift(np.fft.fft(combined_IQ))
+freqs_v2 = np.fft.fftshift(np.fft.fftfreq(num_interp_samples, d=sample_period_interp))
+
+
 # Generate frequency axis and take FFT
 freq = np.fft.fftfreq(n=num_interp_samples, d=sample_period_interp)
 
@@ -249,9 +254,10 @@ plt.subplot(2, 1, 2)
 plt.title("Doppler spectrum of signal")
 plt.xlabel("Frequency [Hz]")
 plt.ylabel("Power [dB]")
-# plt.xlim(-1000, 1000)
+plt.xlim(-1000, 1000)
 # get the power spectrum
-plt.plot(IQ_freq, 20*np.log(np.abs(doppler_spectrum[0])))
+#plt.plot(IQ_freq, 20*np.log(np.abs(doppler_spectrum[0])))
+plt.plot(freqs_v2, np.abs(doppler_spectrum_v2))  # in kHz
 plt.legend(["Doppler spectrum"])
 plt.tight_layout()
 plt.show()
