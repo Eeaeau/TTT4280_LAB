1e668d2b8a59b416aad9c5ce411e9dcb4f857830
Sebastian Skogen Raa <40955082+Eeaeau@users.noreply.github.com>
2021-03-16 16:16:51 +0100

    Update plot_roi.py

--- Lab4/plot_roi.py
+++ Lab4/plot_roi.py
@@ -14,15 +14,24 @@ def import_and_format(path, fps):
     with open(path, 'r') as fid:
         lines = fid.readlines()
         i=0
-        data = np.empty([len(lines),3], dtype=float)
         total_images = len(lines)
+
+        data = np.empty([3, total_images], dtype=float)
+        r = np.empty(total_images, dtype=float)
+        g = np.empty(total_images, dtype=float)
+        b = np.empty(total_images, dtype=float)
         for line in lines:
             line = [float(e) for e in line.split()]
             # print(line)
-            data[i] = line
+            r[i] = line[0]
+            g[i] = line[1]
+            b[i] = line[2]
             i+=1
-        # print(len(lines))
 
+        # print(len(lines))
+        data[0] = r
+        data[1] = g
+        data[2] = b
         # data = np.fromfile(fid)
         # # data = data.reshape((-1, 3))
         t = np.linspace(0,30, total_images)
@@ -36,5 +45,6 @@ def import_and_format(path, fps):
 # t, data = import_and_format(path, fps)
 t, data = import_and_format(path, fps)
 
-print(data.shape)
-# plt.plot(t, data[0])
+print(data[0])
+plt.plot(t, data[0])
+plt.show()
