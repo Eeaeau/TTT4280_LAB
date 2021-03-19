2c6a8eb1446a7f536f8e2ddc054c1c1b052bad46
Sebastian <sebskogen@gmail.com>
2021-03-19 10:28:05 +0100

    Update plot_roi.py

--- Lab4/plot_roi.py
+++ Lab4/plot_roi.py
@@ -58,7 +58,7 @@ output = signal.filtfilt(b, a, data[1])
 plt.plot(t, output, label='filtered_lp')
 
 fc_hp = 0.5  # Cut-off frequency of the filter
-w = fc_hp / (fps / 2) # Normalize the frequency
+w = fc_hp / (fps / 2)  # Normalize the frequency
 b, a = signal.butter(2, w, 'highpass')
 output_hp = signal.filtfilt(b, a, output)
 plt.plot(t, output_hp, label='filtered_lphp')
@@ -68,5 +68,6 @@ plt.show()
 
 # plt.subplots(2, 1, 2)
 
-plt.magnitude_spectrum(output, 1/fps, scale='dB')
+plt.magnitude_spectrum(output, fps, window=np.hamming(
+    len(output)), pad_to=len(output)+100, scale='dB')
 plt.show()
