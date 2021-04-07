e8bfa7603928a2ce1f05ee3cdb3882e913116618
Sebastian <sebskogen@gmail.com>
2021-04-07 15:13:03 +0200

    Updated exports from lab1

---
+++ Lab1/Scripts/Simple_analyse_data.py
@@ -0,0 +1,114 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from numpy.core.fromnumeric import shape
+import scipy.signal as signal
+
+
+# ---------- constants
+channels = 4
+max_voltage = 1
+adc_res = 4096
+
+# ----------- functions
+
+
+def raspi_import(path, channels=4):
+    """
+    Import data produced using adc_sampler.c.
+    Returns sample period and ndarray with one column per channel.
+    Sampled data for each channel, in dimensions NUM_SAMPLES x NUM_CHANNELS.
+    """
+
+    with open(path, 'r') as fid:
+        sample_period = np.fromfile(fid, count=1, dtype=float)[0]
+        data = np.fromfile(fid, dtype=np.uint16)
+        data = data.reshape((-1, channels))
+    return sample_period, data
+
+
+def format_and_plot(string):
+    # Import data from bin file
+    sample_period, data = raspi_import(
+        string, channels)
+
+    sample_period *= 1e-6  # change unit to micro seconds
+
+    print("Sampling frequency:", 1/sample_period)
+
+    # Generate time axis
+    num_of_samples = data.shape[0]  # returns shape of matrix
+    t = np.linspace(start=0, stop=num_of_samples *
+                    sample_period, num=num_of_samples)
+
+    # define new constants
+    elements_removed = int(31250*.5)
+
+    sample_duration = num_of_samples/31250
+
+    num_of_samples_fixed = num_of_samples - elements_removed
+
+    num_interp_samples = int((num_of_samples_fixed)*1)
+
+    sample_period_interp = (sample_duration-sample_period *
+                            elements_removed)/num_interp_samples
+
+    # ----- Defining a new data arrays ----
+
+    # fixed array by removing first samples
+    data_fixed = np.empty([channels, num_of_samples_fixed])
+    # interperated array for upscaling
+    data_interp = np.empty([channels, num_interp_samples])
+    # define new time array
+    t_interp = np.linspace(start=0, stop=num_interp_samples*sample_period_interp,
+                           num=num_interp_samples)
+
+    for i in range(channels):
+        data_fixed[i] = data[:, i][elements_removed:]
+        data_interp[i] = np.interp(
+            t_interp, t[:num_of_samples_fixed], data_fixed[i])
+
+    for i in range(channels):
+        # removes DC component for each channel
+        data_interp[i] = signal.detrend(data_interp[i], axis=0)
+
+    # ------------- Plotting - ---------------
+    pad_width = num_interp_samples+2*16
+
+    fig = plt.figure(figsize=(16/1.8, 9/1.8))
+
+    # channel_names = ["Ch1@$0.993V$", "Ch2@$0.689V$","Ch3@$0.386V$", "Ch4@$11.6mV$"]
+    channel_names = ["Ch1", "Ch2", "Ch3", "Ch4"]
+
+    # time
+    plt.subplot(2, 1, 1)
+    plt.title("Tidsdomene av signal")
+    plt.xlabel("Tid [us]")
+    plt.ylabel("Spenning")
+    plt.grid(True)
+    plt.xlim(0, .001)
+    for i in range(0, channels):
+        plt.plot(t_interp, data_interp[i]/adc_res*max_voltage)
+
+    # 1VA+1V 2.54Vdd, 500Hz
+    # plt.legend(["Ch1", "Ch2", "Ch3", "Ch4"])
+    plt.legend(channel_names, loc="center left")
+
+    # power spectrum
+    plt.subplot(2, 1, 2)
+    plt.title("Doppler spektrum av signal")
+
+    for i in range(channels):
+        plt.magnitude_spectrum(
+            data_interp[i]/adc_res*max_voltage, Fs=1/sample_period_interp, pad_to=pad_width, scale="dB")  # in kHz
+
+    plt.legend(channel_names, loc="center left")
+    plt.xlabel("Frekvens [Hz]")
+    plt.ylabel("Magnitude [dB]")
+    plt.xlim(11*10**3, 11.5*10**3)
+    plt.tight_layout()
+    plt.show()
+
+
+# ---------------- run
+
+format_and_plot('Lab1\Scripts\export\measurement@20kHz.bin')
